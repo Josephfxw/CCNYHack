41cdaeb7a7b9b652564a54595bc34357a0b06f6f
Josephfxw <Josephxwf@gmail.com>
2016-04-23 16:43:20 -0400

    add db

--- web/index.php
+++ web/index.php
@@ -81,10 +81,8 @@ $app->post('/volunteerUserCheck.html', function() use($app) {
     $warning4 = "Re-enter Password";
       foreach ($names as $value) {
 
-      if ($value['name'] == $username){
-        $warning1 = "User alrready exists!"
-      }
-    
+      
+
 
      }
      return $app['twig']->render('signup.html', array(
