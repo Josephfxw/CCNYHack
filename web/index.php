14bda7da3a112223b42d8c7712da3199fbd15932
Josephfxw <Josephxwf@gmail.com>
2016-04-23 11:04:29 -0400

    add db

--- web/index.php
+++ web/index.php
@@ -68,13 +68,6 @@ if (count($names)>0){
   ));
 
  }
- else {
-   $st = $app['pdo']->prepare('CREATE table test_tableb (id integer, name text)');
-   $st->execute();
-   $st = $app['pdo']->prepare('INSERT into test_tableb values (1, 'hello database')');
-   $st->execute();
-   return $app['twig']->render('signin.html');
- }
 
 
 
