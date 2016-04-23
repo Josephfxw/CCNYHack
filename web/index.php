63f43c0135c39e66b0e7868885b7fa335e589db8
Josephfxw <Josephxwf@gmail.com>
2016-04-23 13:08:29 -0400

    add db

--- web/index.php
+++ web/index.php
@@ -75,7 +75,7 @@ if (count($names)>0){ # table exixts
       ));
     }
 }
-  $st1 = $app['pdo']->prepare('INSERT into users_table ( name , email, password) values ('$_POST["username"]','$_POST["email"]','$_POST["password1"]'');
+  $st1 = $app['pdo']->prepare("INSERT into users_table ( name , email, password) values ('$_POST["username"]','$_POST["email"]','$_POST["password1"]'");
   $st1->execute();
 
   $st2 = $app['pdo']->prepare('SELECT name FROM users_table');
