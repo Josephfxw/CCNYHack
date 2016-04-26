776bb5bfd42b433c29024d79b5a09c3a16cb5fd7
Josephfxw <Josephxwf@gmail.com>
2016-04-26 15:44:38 -0400

    edit layout

--- web/volunteerProfileEdit.php
+++ web/volunteerProfileEdit.php
@@ -23,6 +23,44 @@
         <script src="js/navbarVolunteerProfileEdit.js"></script>
 
         <?php
+        require('../vendor/autoload.php');
+
+        $app = new Silex\Application();
+        $app['debug'] = true;
+
+        // Register the monolog logging service
+        $app->register(new Silex\Provider\MonologServiceProvider(), array(
+          'monolog.logfile' => 'php://stderr',
+        ));
+        // Register view rendering
+        $app->register(new Silex\Provider\TwigServiceProvider(), array(
+            'twig.path' => __DIR__.'/views',
+        ));
+        
+        $dbopts = parse_url(getenv('DATABASE_URL'));
+        $app->register(new Herrera\Pdo\PdoServiceProvider(),
+                       array(
+                           'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+                           'pdo.username' => $dbopts["user"],
+                           'pdo.password' => $dbopts["pass"]
+                       )
+        );
+
+
+        $app->get('/db2/', function() use($app) {
+          $st = $app['pdo']->prepare('SELECT name FROM volunteerUsers_table');
+          $st->execute();
+
+          $names = array();
+          while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+            $app['monolog']->addDebug('Row ' . $row['name']);
+            $names[] = $row;
+          }
+
+          return $app['twig']->render('database.twig', array(
+            'names' => $names
+          ));
+        });
                 // include ImageManipulator class
                 require_once('ImageManipulator.php');
 
