70545560d807f4e5b4ba9090d494504283947ce2
Josephfxw <Josephxwf@gmail.com>
2016-04-25 21:45:51 -0400

    add edit profile

--- web/upload.php
+++ web/upload.php
@@ -11,11 +11,12 @@ if ($_FILES['fileToUpload']['error'] > 0) {
     // get extension of the uploaded file
     $fileExtension = strrchr($_FILES['fileToUpload']['name'], ".");
     // check if file Extension is on the list of allowed ones
-    echo $fileExtension;
+
     if (in_array($fileExtension, $validExtensions)) {
         // we are renaming the file so we can upload files with the same name
         // we simply put current timestamp in fron of the file name
         $newName = time() . '_' . $_FILES['fileToUpload']['name'];
+        echo $newName
         $destination = 'uploads/' . $newName;
         if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $destination)) {
             echo 'File ' .$newName. ' succesfully copied';
