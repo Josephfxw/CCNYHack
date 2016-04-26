b36ea9bcc3a4e2952e13d382cf80e2415711c6ec
Josephfxw <Josephxwf@gmail.com>
2016-04-25 22:58:10 -0400

    add edit profile

--- web/upload.php
+++ web/upload.php
@@ -17,13 +17,13 @@ if ($_FILES['fileToUpload']['error'] > 0) {
         // we simply put current timestamp in fron of the file name
         $newName = time() . '_' . $_FILES['fileToUpload']['name'];
         echo "<br />".$newName;
-        $destination = 'uploads/' . $_FILES['fileToUpload']['name'];
+        $destination = 'web/uploads/' . $_FILES['fileToUpload']['name'];
 
         if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $destination)) {
             echo "succesfully copied";
         }
     } else {
-        echo 'You must upload an image...';
+        echo "You must upload an image";
     }
 }
 
