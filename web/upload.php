7b1cb5252c275404e424fabc7d364170bb279e23
Josephfxw <Josephxwf@gmail.com>
2016-04-25 21:39:05 -0400

    add edit profile

--- web/upload.php
+++ web/upload.php
@@ -2,6 +2,10 @@
 if ($_FILES['fileToUpload']['error'] > 0) {
     echo "Error: " . $_FILES['fileToUpload']['error'] . "<br />";
 } else {
+    echo "File name: " . $_FILES['fileToUpload']['name'] . "<br />";
+    echo "File type: " . $_FILES['fileToUpload']['type'] . "<br />";
+    echo "File size: " . ($_FILES['fileToUpload']['size'] / 1024) . " Kb<br />";
+    echo "Temp path: " . $_FILES['fileToUpload']['tmp_name'];
     // array of valid extensions
     $validExtensions = array('.jpg', '.jpeg', '.gif', '.png');
     // get extension of the uploaded file
