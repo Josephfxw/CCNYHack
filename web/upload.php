20976bf6158398d0108ec0e5b5c31798f3cca208
Josephfxw <Josephxwf@gmail.com>
2016-04-25 21:36:44 -0400

    add edit profile

--- web/upload.php
+++ web/upload.php
@@ -5,7 +5,7 @@ if ($_FILES['fileToUpload']['error'] > 0) {
     // array of valid extensions
     $validExtensions = array('.jpg', '.jpeg', '.gif', '.png');
     // get extension of the uploaded file
-    $fileExtension = strrchr($_FILES['fileToUpload']['name'], ".");
+    $fileExtension = strrchr($_FILES['fileToUpload']['type'], ".");
     // check if file Extension is on the list of allowed ones
     if (in_array($fileExtension, $validExtensions)) {
         // we are renaming the file so we can upload files with the same name
