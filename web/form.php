9a320fc5340f90a2e354178bcde4a001d82f149e
Josephfxw <Josephxwf@gmail.com>
2016-04-26 11:05:06 -0400

    commit

--- web/form.php
+++ web/form.php
@@ -4,7 +4,7 @@
     <title>Upload Files using normal form and PHP</title>
 </head>
 <body>
-  <form enctype="multipart/form-data" method="post" action="upload.php">
+  <form enctype="multipart/form-data" method="post" action="upload2.php">
     <div class="row">
       <label for="fileToUpload">Select a File to Upload</label><br />
       <input type="file" name="fileToUpload" id="fileToUpload" />
