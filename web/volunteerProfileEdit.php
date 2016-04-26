899740337a67267a929eb7b64b75c7a6ef597d05
Josephfxw <Josephxwf@gmail.com>
2016-04-26 14:32:11 -0400

    commit

--- web/volunteerProfileEdit.php
+++ web/volunteerProfileEdit.php
@@ -22,46 +22,46 @@
     <body>
         <script src="js/navbarVolunteer.js"></script>
 
-<?php
-        // include ImageManipulator class
-        require_once('ImageManipulator.php');
+        <?php
+                // include ImageManipulator class
+                require_once('ImageManipulator.php');
 
-        if ($_FILES['fileToUpload']['error'] > 0) {
-            echo "Error: " . $_FILES['fileToUpload']['error'] . "<br />";
-        } else {
-            // array of valid extensions
-            $validExtensions = array('.jpg', '.jpeg', '.gif', '.png');
-            // get extension of the uploaded file
-            $fileExtension = strrchr($_FILES['fileToUpload']['name'], ".");
-            // check if file Extension is on the list of allowed ones
-            if (in_array($fileExtension, $validExtensions)) {
-                $newNamePrefix = time() . '_';
-                $manipulator = new ImageManipulator($_FILES['fileToUpload']['tmp_name']);
-                $width  = $manipulator->getWidth();
-                $height = $manipulator->getHeight();
-                $centreX = round($width / 2);
-                $centreY = round($height / 2);
-                // our dimensions will be 200x130
-                $x1 = $centreX - 100; // 200 / 2
-                $y1 = $centreY - 65; // 130 / 2
+                if ($_FILES['fileToUpload']['error'] > 0) {
+                    echo "Error: " . $_FILES['fileToUpload']['error'] . "<br />";
+                } else {
+                    // array of valid extensions
+                    $validExtensions = array('.jpg', '.jpeg', '.gif', '.png');
+                    // get extension of the uploaded file
+                    $fileExtension = strrchr($_FILES['fileToUpload']['name'], ".");
+                    // check if file Extension is on the list of allowed ones
+                    if (in_array($fileExtension, $validExtensions)) {
+                        $newNamePrefix = time() . '_';
+                        $manipulator = new ImageManipulator($_FILES['fileToUpload']['tmp_name']);
+                        $width  = $manipulator->getWidth();
+                        $height = $manipulator->getHeight();
+                        $centreX = round($width / 2);
+                        $centreY = round($height / 2);
+                        // our dimensions will be 200x130
+                        $x1 = $centreX - 100; // 200 / 2
+                        $y1 = $centreY - 65; // 130 / 2
 
-                $x2 = $centreX + 100; // 200 / 2
-                $y2 = $centreY + 65; // 130 / 2
+                        $x2 = $centreX + 100; // 200 / 2
+                        $y2 = $centreY + 65; // 130 / 2
 
-                // center cropping to 200x130
-                $newImage = $manipulator->crop($x1, $y1, $x2, $y2);
-                // saving file to uploads folder
-                $manipulator->save('uploads/' . $_FILES['fileToUpload']['name']);
-                echo 'Done ...';
-            } else {
-                echo 'You must upload an image...';
-            }
-        }
+                        // center cropping to 200x130
+                        $newImage = $manipulator->crop($x1, $y1, $x2, $y2);
+                        // saving file to uploads folder
+                        $manipulator->save('uploads/' . $_FILES['fileToUpload']['name']);
+                        echo 'Done ...';
+                    } else {
+                        echo 'You must upload an image...';
+                    }
+                }
 
 
-        echo '<img src="uploads/bird.jpeg" alt="Smiley face" height="42" width="42">';
-        echo '<img src="uploads/bird.png" alt="Smiley face" height="42" width="42">';
-?>
+
+                echo '<img src="uploads/bird.png" alt="Smiley face" height="42" width="42">';
+        ?>
 
 
     <hr class="">
@@ -82,6 +82,8 @@
   <div class="panel panel-default">
     <div class="panel-heading">Public Profile</div>
     <div>
+      <h5> Profile picture</h5>
+      <?php echo '<img src="uploads/bird.jpeg" alt="Smiley face" height="42" width="42">'; ?>
       <form enctype="multipart/form-data" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
 
@@ -89,6 +91,18 @@
 
 
           <input type="submit" value="Upload" />
+
+      </form>
+      <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+
+          Name: <input type="text" name="name" value = "Joe"><br>
+          Public Email: <input type="email" name="email" value = "Josephxwf@gmail.com"><br>
+          Location:<input type="text" name="location" value = "New York"><br>
+          Avaliable Time:<input type="text" name="avaliableTime" value = "Mon-Wed 4-6pm"><br>
+
+
+          <input type="submit" value="Save" />
 
       </form>
     </div>
