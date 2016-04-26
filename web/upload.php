19dedc7e6d3cb2316c8fd8b8907d92c6a9b9e5b5
Josephfxw <Josephxwf@gmail.com>
2016-04-25 21:41:42 -0400

    add edit profile

--- web/upload.php
+++ web/upload.php
@@ -9,8 +9,9 @@ if ($_FILES['fileToUpload']['error'] > 0) {
     // array of valid extensions
     $validExtensions = array('.jpg', '.jpeg', '.gif', '.png');
     // get extension of the uploaded file
-    $fileExtension = strrchr($_FILES['fileToUpload']['type'], ".");
+    $fileExtension = strrchr($_FILES['fileToUpload']['name'], ".");
     // check if file Extension is on the list of allowed ones
+    echo $fileExtension
     if (in_array($fileExtension, $validExtensions)) {
         // we are renaming the file so we can upload files with the same name
         // we simply put current timestamp in fron of the file name
