b883180dd89b35947e79cdabce0c8d5f627414c5
Josephfxw <Josephxwf@gmail.com>
2016-04-26 10:32:18 -0400

    commit

--- web/upload2.php
+++ web/upload2.php
@@ -11,12 +11,15 @@ if ($_FILES['fileToUpload']['error'] > 0) {
     $fileExtension = strrchr($_FILES['fileToUpload']['name'], ".");
     // check if file Extension is on the list of allowed ones
     if (in_array($fileExtension, $validExtensions)) {
-      echo "hiiii";
+
         $newNamePrefix = time() . '_';
+        echo "1";
         $manipulator = new ImageManipulator($_FILES['fileToUpload']['tmp_name']);
         // resizing to 200x200
+        echo "2";
         $newImage = $manipulator->resample(200, 200);
         // saving file to uploads folder
+        echo "3";
         $manipulator->save('uploads/' . $newNamePrefix . $_FILES['fileToUpload']['name']);
         echo 'Done ...';
     } else {
